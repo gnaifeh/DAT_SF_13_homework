75f6445003e56d89686ddea2dc6a8c2217e0e1a0
gnaifeh <gpnaifeh@gmail.com>
2015-05-03 01:38:45 -0700

    sunday may 3 early morning commit - changes to mysql data file

--- final_project/dev_notes/api_notes/query_data_file.py
+++ final_project/dev_notes/api_notes/query_data_file.py
@@ -2,4 +2,4 @@
 gmaps_api_key = "AIzaSyCE9krxNevRUd6UXBppj9l60eQLW1tYInU"
 #data for querying bing maps
 #https://www.bingmapsportal.com/Application
-mmaps_api_key = "An0tPT-fNOUwKKyBWqqw6WdXZCFU929vEssbqMB_i6ADsh5aRUe80zgc_puMiFNQ"
+mmaps_api_key = "Ai6RPZEkZprtSPQZmZHMWfmr2-IxM_xGdh-5Ln4ataxhYVE9U7RHpmKEdFN7Ne3G"
